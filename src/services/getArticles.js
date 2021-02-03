b568edca77697cd59cfa11f7ad3fd39de4e75b26
Katelinn Jones <katiejonesyo@gmail.com>
2021-02-03 15:52:08 -0800

    syntax cleanup

--- src/services/getArticles.js
+++ src/services/getArticles.js
@@ -3,7 +3,8 @@ const API_URL = `http://newsapi.org/v2/top-headlines?
 country=us&apiKey=${API_KEY}`;
 
 const getArticles = () => {
-  return fetch(API_URL).then((res) => res.json());
+  return fetch(API_URL)
+  .then((res) => res.json());
 };
 
 export default getArticles;
