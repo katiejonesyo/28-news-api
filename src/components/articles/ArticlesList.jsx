6515114ce19069dbc83e3f2ab53fff5361b0eb2f
Katelinn Jones <katiejonesyo@gmail.com>
2021-02-03 15:38:52 -0800

    articles list done and test started

--- src/components/articles/ArticlesList.jsx
+++ src/components/articles/ArticlesList.jsx
@@ -1,11 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Article from '../article/Article';
 
-export default class ArticlesList extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
+const ArticlesList = ({ articles }) => {
+    const articlesList = articles.map((article, index) => {
+        return(
+            <li key={index}>
+                <Article {...article} />
+                <br/>
+            </li>
         )
-    }
+    });
+
+    return <ul style={{ listStyle: 'none' }}>{articlesList}</ul>;
+};
+
+
+ArticlesList.propTypes = {
+    articles: PropTypes.arrayOf(
+        PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            author: PropTypes.string,
+            description: PropTypes.string.isRequired,
+        }).isRequired
+    )
 }
+
+export default ArticlesList;
+
+
